cbebdd727fd27c3d6ab8167cca1023dceff35703
juliabb1 <j.bai@hotmail.de>
2020-12-18 17:25:21 +0100

    del implemented

--- HA03_linkedListLib.c
+++ HA03_linkedListLib.c
@@ -26,38 +26,69 @@ void addListElem(listElement *start){
     scanf("%s", &new->firstName);
     printf("Please enter age: \n");
     scanf("%d", &new->age);
-    printf("end of function\n");
+    printf("\n\n");
 }
 
 void printList(listElement *start){
 
-    if(start->nextElem == NULL) {
-        printf("Empty list.\n\n");
+    if(start -> nextElem == NULL) {
+        printf("Empty list.\n");
+        return;
     }
+
+    printf("**********************************\n");
     while(start->nextElem != NULL) {
         start = start->nextElem;
         printf("Last name: %s \n", start -> lastName);
-        printf("Fitst name: %s \n", start -> firstName);
-        printf("age: %d \n", start -> age);
+        printf("First name: %s \n", start -> firstName);
+        printf("Age: %d \n", start -> age);
+        printf("\n");
     }
+    printf("**********************************\n");
 
 }
 
 void delListElem(listElement *start){
 
-    /* YOUR CODE HERE */
-    /* ---------------*/
+    if(start->nextElem == NULL) {                                             //when list ist empty
+        printf("Empty list.\n\n");
+        return;
+    }
 
-    printf("\n>> delListElem fcn is tbd.\n\n");
+    char remove_lastName[50];
+    listElement *currElem = start;
+    listElement *delElem;
 
+    printf("Last name of the person you want to remove: ");
+    scanf("%s", &remove_lastName);
+    
+    while(strcmp(currElem -> nextElem->lastName, remove_lastName) != 0){        //searches for the element that should be deleted
+        currElem = currElem->nextElem;                                          //currElem points to the element before the to be deleted list
+    }
+    delElem = currElem -> nextElem;                                             //delElem points to the element that should be deleted
+    currElem -> nextElem = delElem -> nextElem;                                 //currElem points to nextElem which now ist the Elem after delElem
+    free(delElem);
+    printf("Succesfully deleted!\n\n");
 }
 
 void delList(listElement *start){
 
-    /* YOUR CODE HERE */
-    /* ---------------*/
+    if(start -> nextElem == NULL) {
+        printf("List is already empty.\n\n");
+        return;
+    }
 
-    printf("\n>> getLenOfList fcn is tbd.\n\n");
+    listElement *currElem = start;
+    listElement *delElem;
+
+    while(currElem -> nextElem != NULL) {
+        delElem = currElem -> nextElem;
+        currElem -> nextElem = delElem -> nextElem;
+        free(delElem);
+    }
+
+    printf("List succesfully deleted!\n\n");
+
 
 }
 
